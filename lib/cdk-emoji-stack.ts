81e23afd9da2d6848287fb829da16866c82b247b
Lloyd McKie <lloyd.mckie+git@gmail.com>
2022-08-13 10:26:54 +0800

    feat: add emoji

--- lib/cdk-emoji-stack.ts
+++ lib/cdk-emoji-stack.ts
@@ -1,5 +1,7 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Queue, QueueProps } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import {emoji} from "node-emoji";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkEmojiStack extends Stack {
@@ -9,8 +11,15 @@ export class CdkEmojiStack extends Stack {
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'CdkEmojiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new QueueMailBox(this, 'CdkEmojiQueue', {
+      visibilityTimeout: Duration.seconds(300)
+    });
+  }
+}
+
+export class QueueMailBox extends Queue {
+  constructor(scope: Construct, id: string, props?: QueueProps){
+    super(scope, id, props);
+ console.log("creating queue ", emoji.mailbox_with_mail);
   }
 }
